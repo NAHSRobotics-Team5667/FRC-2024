b6f11853f62c6253fed97863bac98b244b9c00a2
DEV-DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2024-01-07 16:55:41 -0500

    feat: libraries that aren't supported yet

--- src/main/java/frc/robot/commands/Testing.java
+++ src/main/java/frc/robot/commands/Testing.java
@@ -5,16 +5,39 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.DrivetrainSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public final class Testing {
-  /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(DrivetrainSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+/** An example command that uses an example subsystem. */
+public class Testing extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DrivetrainSubsystem m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public Testing(DrivetrainSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
 
-  private Testing() {
-    throw new UnsupportedOperationException("This is a testing class!");
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }
