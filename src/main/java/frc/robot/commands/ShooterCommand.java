bd964dd550aee73d0b911e5eaed75f6add88378a
Budgerigar295 <samardude67@gmail.com>
2024-02-07 19:10:19 -0500

    Update ShooterCommand.java

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterCommand extends Command {
   /** Creates a new ShooterCommand. */
@@ -14,15 +15,20 @@ public class ShooterCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //Starts Motors
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
