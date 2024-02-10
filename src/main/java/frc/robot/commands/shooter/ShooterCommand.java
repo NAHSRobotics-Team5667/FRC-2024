ea618b60b450d2bce1e0159b7b7d30e1d1baef84
Budgerigar295 <samardude67@gmail.com>
2024-02-10 14:15:09 -0500

    Adding to shooNote.java

--- src/main/java/frc/robot/commands/shooter/ShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterCommand.java
@@ -6,16 +6,21 @@ package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.util.States.ShooterStates;
 
 public class ShooterCommand extends Command {
+
+    public ShooterSubsystem shooter;
     /** Creates a new ShooterCommand. */
     public ShooterCommand() {
         // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(shooter);
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+
     }
 
     // Called every time the scheduler runs while the command is scheduled.
--- src/main/java/frc/robot/commands/shooter/shootNote.java
+++ src/main/java/frc/robot/commands/shooter/shootNote.java
@@ -2,6 +2,7 @@ package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.util.States.ShooterStates;
 
 /**
  * This command set the speed of the motor on the intake to 100% and shoot out
@@ -11,6 +12,9 @@ import frc.robot.subsystems.ShooterSubsystem;
 public class shootNote extends Command {
 
     public ShooterSubsystem shooter;
+    public ShooterStates shooterState;
+     public double[] shooterRPMs = shooter.getShooterRPM();
+
 
     // * Creates a new Shooter. */
     public shootNote() {
@@ -23,18 +27,36 @@ public class shootNote extends Command {
     // Called when command is initiated/first scheduled
     @Override
     public void initialize() {
-
+        shooterState = ShooterStates.STOPPED;
     }
 
     // Called when scheduler runs while the command is scheduled
     @Override
     public void execute() {
-
+        shooter.setShooterSpeed(1.00);
+        
+        //Checking Shooter RPMs until it reaches Max RPM to set Shooter State to FULL_SPEED
+        while (shooterRPMs[0] <= 5900 && shooterRPMs[1] <= 5900) {
+            shooterState = ShooterStates.SPEEDING_UP;
+        }
+        shooterState = ShooterStates.FULL_SPEED;
+        
     }
 
     // Called when the command is interruped or ended
     @Override
     public void end(boolean interrupted) {
+        boolean hasGamePiece = shooter.hasGamePiece();        
+        if (hasGamePiece == false) {
+            shooter.setShooterSpeed(0.00);
+
+            //Checking Shooter RPMs until its at 0 to set Shooter State to STOPPED
+            while (shooterRPMs[0] > 0 && shooterRPMs[1] > 0) {
+                shooterState = ShooterStates.SLOWING_DOWN;
+        }
+            shooterState = ShooterStates.STOPPED;
+
+        }
 
     }
 
