827f7454e82463c09f491b82a644dcd27bb9e3bd
Budgerigar295 <samardude67@gmail.com>
2024-02-10 15:11:38 -0500

    Deleted duplicate file and added its code into ShooterCommand.java

--- src/main/java/frc/robot/commands/shooter/ShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterCommand.java
@@ -11,31 +11,51 @@ import frc.robot.util.States.ShooterStates;
 public class ShooterCommand extends Command {
 
     public ShooterSubsystem shooter;
+    public ShooterStates shooterState;
+    public double[] shooterRPMs = shooter.getShooterRPM();
     /** Creates a new ShooterCommand. */
     public ShooterCommand() {
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(shooter);
     }
 
-    // Called when the command is initially scheduled.
+    // Called when command is initiated/first scheduled
     @Override
     public void initialize() {
-
+        shooterState = ShooterStates.STOPPED;
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
+    // Called when scheduler runs while the command is scheduled
     @Override
     public void execute() {
-        // Starts Motors
+        shooter.setShooterSpeed(1.00);
+        
+        //Checking Shooter RPMs until it reaches Max RPM to set Shooter State to FULL_SPEED
+        while (shooterRPMs[0] <= 5900 && shooterRPMs[1] <= 5900) {
+            shooterState = ShooterStates.SPEEDING_UP;
+        }
+        shooterState = ShooterStates.FULL_SPEED;
+        
     }
 
-    // Called once the command ends or is interrupted.
+    // Called when the command is interruped or ended
     @Override
     public void end(boolean interrupted) {
+        boolean hasGamePiece = shooter.hasGamePiece();        
+        if (hasGamePiece == false) {
+            shooter.setShooterSpeed(0.00);
+
+            //Checking Shooter RPMs until its at 0 to set Shooter State to STOPPED
+            while (shooterRPMs[0] > 0 && shooterRPMs[1] > 0) {
+                shooterState = ShooterStates.SLOWING_DOWN;
+        }
+            shooterState = ShooterStates.STOPPED;
+
+        }
 
     }
 
-    // Returns true when the command should end.
+    // Called so it should return true when the command will end
     @Override
     public boolean isFinished() {
         return false;
